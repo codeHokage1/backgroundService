1b9a10401558abdad012f8c1e9e4c81908ce53ec
Farhan Sodiq <sodiqfarhan360@gmail.com>
2024-08-21 18:47:50 +0100

    Created a background service

---
+++ TestingBgServices/SampleBackgroundService.cs
@@ -0,0 +1,21 @@
+﻿
+namespace TestingBgServices
+{
+    public class SampleBackgroundService : BackgroundService
+    {
+        private readonly ILogger<BackgroundService> _logger;
+
+        public SampleBackgroundService(ILogger<BackgroundService> logger)
+        {
+            _logger = logger;
+        }
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
